cbacda6a6f91d4a21f6db5d4a3b5593dc51f9f39
bafeimao <29283212@qq.com>
2015-10-13 14:49:13 +0800

    add some files

---
+++ spring-examples/src/main/java/ktgu/examples/spring/beanPostProcessor/Foo.java
@@ -0,0 +1,7 @@
+package ktgu.examples.spring.beanPostProcessor;
+
+/**
+ * Created by ktgu on 15/8/17.
+ */
+public class Foo {
+}
---
+++ spring-examples/src/main/java/ktgu/examples/spring/beanPostProcessor/Main.java
@@ -0,0 +1,22 @@
+package ktgu.examples.spring.beanPostProcessor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by ktgu on 15/8/17.
+ */
+public class Main {
+    static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        String path = Main.class.getResource(".").getPath();
+
+        logger.info("path:{}", path);
+
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext(path + "context.xml");
+        context.close();
+    }
+}
