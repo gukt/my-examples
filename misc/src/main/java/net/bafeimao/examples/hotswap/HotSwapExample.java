a8fb6046fbbf0fbb5f792358df2c2099a42b822c
bafeimao <29283212@qq.com>
2015-12-17 18:17:03 +0800

    misc updates

--- misc/src/main/java/net/bafeimao/examples/hotswap/HotSwapExample.java
+++ misc/src/main/java/net/bafeimao/examples/hotswap/HotSwapExample.java
@@ -14,6 +14,9 @@ public class HotSwapExample {
     public static void main(String[] args) throws Exception {
         HotSwapExample example = new HotSwapExample();
 
+        assert example == null;
+
+
         while (true) {
             example.systemLoad();
             example.hotswapLoad();
